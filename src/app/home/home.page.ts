d0aa68c5133474b6462e93ffde57e413f779154d
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-04-04 17:03:51 +0200

    Added movie detail screen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,16 +16,19 @@ export class HomePage {
     constructor() {
         this.slides = [];
         this.slides.push({
+            id: 1,
             title: 'Capitana Marvel',
             ratio: '+7',
             img: 'https://s3-eu-west-1.amazonaws.com/disasterdraw.blindcat/peliculas/000/004/310/original/capitana_marvel_75077.jpg?1551918359'
         } as SliderModel);
         this.slides.push({
+            id: 2,
             title: 'Dolor y gloria',
             ratio: '+16',
             img: 'https://s3-eu-west-1.amazonaws.com/disasterdraw.blindcat/peliculas/000/004/338/original/dolor_y_gloria_75206.jpg?1553211412'
         } as SliderModel);
         this.slides.push({
+            id: 3,
             title: 'Dumbo',
             ratio: 'Apta',
             img: 'https://s3-eu-west-1.amazonaws.com/disasterdraw.blindcat/peliculas/000/004/317/original/dumbo_73862.jpg?1553720388'
--- src/core/models/slider.model.ts
+++ src/core/models/slider.model.ts
@@ -1,4 +1,5 @@
 export interface SliderModel {
+    id: number;
     title: string;
     img: string;
     ratio?: string;
