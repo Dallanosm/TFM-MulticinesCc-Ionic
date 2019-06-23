a370219a6b204acbf1446917ada69b5ac42eacc4
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-06-23 20:54:57 +0200

    Added detail screen

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieStoreService} from '../services/stores/movie-store.service';
+import {ActivatedRoute} from '@angular/router';
+import {MovieDetail} from '../../core/models/movieModel';
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.page.html',
-  styleUrls: ['./movie-details.page.scss'],
+    selector: 'app-movie-details',
+    templateUrl: './movie-details.page.html',
+    styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+    public movieDetail: MovieDetail;
 
-  ngOnInit() {
-  }
+    constructor(private activateRoute: ActivatedRoute, private movieStore: MovieStoreService) {
+        let id = activateRoute.snapshot.paramMap.get('id') as number;
+        movieStore.getMovieDetail(id).then((movieDetails) => {
+            this.movieDetail = movieDetails;
+        });
+    }
+
+    ngOnInit() {
+
+    }
 
 }
--- src/app/services/stores/movie-store.service.ts
+++ src/app/services/stores/movie-store.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {MovieModel} from '../../../core/models/movieModel';
+import {MovieDetail, MovieModel} from '../../../core/models/movieModel';
 import {catchError, map, timeout} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject} from 'rxjs';
@@ -9,7 +9,7 @@ import {BehaviorSubject} from 'rxjs';
 })
 export class MovieStoreService {
 
-    readonly BASE_URL = 'http://localhost:8085';
+    readonly BASE_URL = 'http://llanosmunoz.com:8084';
     readonly TIMEOUT = 20000;
 
     public movieList: BehaviorSubject<Array<MovieModel>>;
@@ -25,16 +25,17 @@ export class MovieStoreService {
         return this.http.get(`${this.BASE_URL}/movies`)
             .pipe(
                 timeout(this.TIMEOUT),
-                map(response => response),
+                map(response => response as Array<MovieModel>),
                 catchError((error) => console.log(error))
             ).toPromise();
     }
+
 
     public getMovieDetail(id: number): Promise<any> {
         return this.http.get(`${this.BASE_URL}/movies/${id}`)
             .pipe(
                 timeout(this.TIMEOUT),
-                map(response => response)
+                map(response => response as MovieDetail)
             ).toPromise();
     }
 }
